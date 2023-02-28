b2a18be3cc4dced383454812ed6dde4cce880ed4
Asherflo <aniyikayetemitopeflorence@gmail.com>
2023-02-28 13:25:16 +0100

    updated

---
+++ static/index.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("application-form");
+
+form.addEventListener("submit", (event) => {
+  // Prevent the form from submitting and refreshing the page
+  event.preventDefault();
+
+  // Get the input values from the form
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+  const phone = form.elements["phone"].value;
+
+  // Construct a request object with the form data
+  const request = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email, phone }),
+  };
+
+  // Send the request to the server
+  fetch("/submit", request)
+    .then((response) => {
+      // Handle the response from the server
+      console.log(response);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      console.log(error);
+    });
+});
